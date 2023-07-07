e04652d7eaeba025e66fd962fd18be6eb7a7911e
Aashna Kshirsagar <ak16499@ad.georgiasouthern.edu>
2023-07-07 12:41:27 -0400

    decision tree, random forest, neural network works + decision tree graph

--- breastCancer.py
+++ breastCancer.py
@@ -1,10 +1,29 @@
 import pandas as pd
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+#import plotly.express as px
+
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+
+
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping
+
+
 
 # Load the dataset
 data = pd.read_csv('breastCancerData.csv')
+data.head()
+
+#px.histogram(data_frame=data, x='diagnosis', color='diagnosis',color_discrete_sequence=['#A865C9','#f6abb6'])
 
 # Separate features (X) and target variable (y)
 X = data.iloc[:, 2:]  # Exclude ID column and use all numeric columns for features
@@ -23,6 +42,73 @@ model.fit(X_train, y_train)
 # Make predictions on the test set
 y_pred = model.predict(X_test)
 
+# Plot the decision tree
+plt.figure(figsize=(12, 8))
+plot_tree(model, feature_names=X.columns, class_names=['Benign', 'Malignant'], filled=True)
+plt.show()
+
 # Evaluate the model
 accuracy = accuracy_score(y_test, y_pred)
-print("Accuracy:", accuracy)
+print("Test accuracy Decision Tree:", accuracy)
+
+# Train the Random Forest classifier
+model = RandomForestClassifier(random_state=42)
+model.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+accuracy = accuracy_score(y_test, y_pred)
+print("Test accuracy Random Forest:", accuracy)
+
+# Scale the features using StandardScaler
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Build the deep neural network model
+model = Sequential()
+model.add(Dense(64, activation='relu', input_shape=(X_train_scaled.shape[1],)))
+model.add(Dropout(0.2))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(1, activation='sigmoid'))
+
+# Compile the model
+model.compile(optimizer=Adam(learning_rate=0.001), loss='binary_crossentropy', metrics=['accuracy'])
+
+# Define early stopping to prevent overfitting
+early_stopping = EarlyStopping(patience=5, restore_best_weights=True)
+
+# Train the model
+history = model.fit(X_train_scaled, y_train, validation_data=(X_test_scaled, y_test),
+                    epochs=2, batch_size=10, callbacks=[early_stopping])
+
+# Evaluate the model
+loss, accuracy = model.evaluate(X_test_scaled, y_test)
+print("Test Loss:", loss)
+print("Test Accuracy:", accuracy)
+
+# Plot the training and validation accuracy over epochs
+plt.plot(history.history['accuracy'], label='Training Accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.title('Training and Validation Accuracy')
+plt.legend()
+plt.show()
